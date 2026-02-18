007eb06de3a1040f72fa83031abf63325879f206
MartinVerniere <mn.verniere@gmail.com>
2026-02-18 10:51:29 -0300

    Completed Exercise 13.3
    Reorganized folders

---
+++ blogs-app/backend/cli.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Sequelize, QueryTypes } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialectOptions: { ssl: false },
+	logging: false,
+});
+
+const main = async () => {
+	try {
+		await sequelize.authenticate();
+
+		const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT });
+		blogs.forEach(blog => {
+			console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
+		});
+
+		sequelize.close();
+	} catch (error) {
+		console.error('Unable to connect to the database:', error)
+	}
+}
+
+main();
