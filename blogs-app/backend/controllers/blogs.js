51fc6c92c00f9a4d21f30dd5e72a65556c633e7f
MartinVerniere <mn.verniere@gmail.com>
2026-02-19 10:15:45 -0300

    Completed Exercise 13.6

--- blogs-app/backend/controllers/blogs.js
+++ blogs-app/backend/controllers/blogs.js
@@ -6,7 +6,7 @@ export const router = express.Router();
 router.get('/', async (req, res) => {
 	const blogs = await Blog.findAll();
 	res.json(blogs);
-})
+});
 
 router.post('/', async (req, res) => {
 	try {
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
 	} catch (error) {
 		return res.status(400).json({ error });
 	}
-})
+});
 
 const blogFinder = async (req, res, next) => {
 	req.blog = await Blog.findByPk(req.params.id);
@@ -28,11 +28,21 @@ router.get('/:id', blogFinder, async (req, res) => {
 	} else {
 		res.status(404).end();
 	}
-})
+});
 
 router.delete('/:id', blogFinder, async (req, res) => {
 	if (req.blog) {
 		await req.blog.destroy();
 	}
 	res.status(204).end();
-})
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+	if (req.blog) {
+		req.blog.likes = (req.blog.likes || 0) + 1;
+		await req.blog.save();
+		res.json(req.blog);
+	} else {
+		res.status(404).end();
+	}
+});
