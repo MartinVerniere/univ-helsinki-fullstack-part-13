d9d9438349336d5156628c88df8a0b6a9c7f2709
MartinVerniere <mn.verniere@gmail.com>
2026-02-20 10:00:03 -0300

    Updated notes app - completed section 13 chapter 3 - "Proper insertion of notes"

--- notes-app/backend/controllers/notes.js
+++ notes-app/backend/controllers/notes.js
@@ -1,23 +1,39 @@
 import express from 'express'
 import models from '../models/index.js';
+import jwt from 'jsonwebtoken';
+import { SECRET } from '../util/config.js';
 const { User, Note } = models;
 
 export const router = express.Router();
+
+const tokenExtractor = (req, res, next) => {
+	const authorization = req.get('authorization');
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		try {
+			req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+		} catch {
+			return res.status(401).json({ error: 'token invalid' });
+		}
+	} else {
+		return res.status(401).json({ error: 'token missing' });
+	}
+	next();
+}
 
 router.get('/', async (req, res) => {
 	const notes = await Note.findAll();
 	res.json(notes);
 })
 
-router.post('/', async (req, res) => {
+router.post('/', tokenExtractor, async (req, res) => {
 	try {
-		const user = await User.findOne();
+		const user = await User.findByPk(req.decodedToken.id);
 		const note = await Note.create({ ...req.body, date: new Date(), userId: user.id });
 		res.json(note);
 	} catch (error) {
 		return res.status(400).json({ error });
 	}
-})
+});
 
 const noteFinder = async (req, res, next) => {
 	req.note = await Note.findByPk(req.params.id);
@@ -30,14 +46,14 @@ router.get('/:id', noteFinder, async (req, res) => {
 	} else {
 		res.status(404).end();
 	}
-})
+});
 
 router.delete('/:id', noteFinder, async (req, res) => {
 	if (req.note) {
 		await req.note.destroy();
 	}
 	res.status(204).end();
-})
+});
 
 router.put('/:id', noteFinder, async (req, res) => {
 	if (req.note) {
@@ -47,4 +63,4 @@ router.put('/:id', noteFinder, async (req, res) => {
 	} else {
 		res.status(404).end();
 	}
-})
+});
