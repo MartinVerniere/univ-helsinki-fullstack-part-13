f9c9aea89b55590f9a330613733032ee51e5d497
MartinVerniere <mn.verniere@gmail.com>
2026-02-24 10:44:12 -0300

    Fixed exercise 13.17: Checkup - v8

--- blogs-app/backend/models/index.js
+++ blogs-app/backend/models/index.js
@@ -4,7 +4,7 @@ import { User } from './user.js';
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-Blog.sync({ alter: true });
-User.sync({ alter: true });
+await User.sync({ alter: true });
+await Blog.sync({ alter: true });
 
 export default { Blog, User }
