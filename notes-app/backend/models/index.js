41ae3e196fcd513c33ace492581e27c180733f59
MartinVerniere <mn.verniere@gmail.com>
2026-02-27 09:11:37 -0300

    Updated notes-app - completed section 13 chapter 4 - "Revisiting many-to-many relationships"

--- notes-app/backend/models/index.js
+++ notes-app/backend/models/index.js
@@ -2,11 +2,15 @@ import { Note } from './note.js';
 import { User } from './user.js';
 import { Team } from './team.js';
 import { Membership } from './membership.js';
+import { UserNotes } from './userNotes.js';
 
 User.hasMany(Note);
 Note.belongsTo(User);
 
-User.belongsToMany(Team, { through: Membership })
-Team.belongsToMany(User, { through: Membership })
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
 
-export default { Note, User, Team, Membership }
+User.belongsToMany(Note, { through: UserNotes, as: 'marked_notes' });
+Note.belongsToMany(User, { through: UserNotes, as: 'users_marked' }); 
+
+export default { Note, User, Team, Membership, UserNotes }
---
+++ notes-app/backend/models/userNotes.js
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize'
+import { sequelize } from '../util/db.js'
+
+export class UserNotes extends Model { }
+
+UserNotes.init({
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	userId: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: { model: 'users', key: 'id' },
+	},
+	noteId: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: { model: 'notes', key: 'id' },
+	},
+}, {
+	sequelize,
+	underscored: true,
+	timestamps: false,
+	modelName: 'user_notes'
+});
